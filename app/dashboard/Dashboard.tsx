66a68692503f38b02f2395712d82ea79986dbaf2
mamenz752 <mamenz752@gmail.com>
2024-09-30 19:23:55 +0900

    wip: sessionの実装

--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -1,36 +1,61 @@
 "use client"
 
-import { useSession, signOut } from "next-auth/react"
-import Link from "next/link"
+import { useSession } from "next-auth/react";
+// import { auth } from "../../auth"
+import { useEffect, useState } from "react";
+// import Link from "next/link"
 
 const DashboardPresenter = () => {
   const {data: session} = useSession();
+  const [isLoading, setIsLoading] = useState(true);
+  const [res, setRes] = useState("");
 
-  if (session) {
+  useEffect(() => {
+    fetch("http://localhost:8000/api/v1/dashboard", {
+      headers: {
+        Authorization: `Bearer ${session?.accessToken}`,
+      }
+    })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP Error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log("res:", data);
+      setRes(data);
+      setIsLoading(false);
+    })
+    .catch((error) => {
+      console.error("fetch error:", error)
+    })
+  }, [session])
+
+  // <div>
+  //     <p>login: {session?.user?.name}</p>
+  //     <p>{session.user?.email}</p>
+  //     <Link href="/login">
+  //     <button
+  //       className="p-8"
+  //       onClick={() => {
+  //         // "use server"
+  //         signOut()
+  //       }
+  //     }
+  //     >
+  //         ログアウト
+  //     </button>
+  //     </Link>
+  // </div>
     return (
-      <div>
-          <p>login: {session?.user?.name}</p>
-          <Link href="/login">
-          <button
-            className="p-8"
-            onClick={() => {
-              // "use server"
-              signOut()
-            }
-          }
-          >
-              ログアウト
-          </button>
-          </Link>
-      </div>
+      isLoading ?
+        <>
+        <p>Not login</p>
+        </> : res
     )
+
   }
 
-  return (
-    <>
-     <p>Not login</p>
-    </>
-  )
-}
 
 export default DashboardPresenter
--- auth.ts
+++ auth.ts
@@ -2,5 +2,54 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [ Google ],
+    providers: [ Google({
+        authorization: {params: {access_type: "offline", prompt: "consent"}}
+    }) ],
+    pages: {
+        signIn: "/login"
+    },
+    session: {
+        strategy: "jwt"
+    },
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account?.provider === "google") {
+                return { ...token, accessToken: account.access_token }
+            }
+
+            // const newAccount = account ?
+            // {
+            //    ...token,
+            //    access_token: account.access_token,
+            //    expires_at: account.expires_at,
+            //    refresh_token: account.refresh_token
+            // } : account
+
+            // return token
+            // else if (Date.now() < token.expires_at * 1000) {
+            //     return token
+            // }
+
+
+            // ユーザーが存在する場合
+            // if (user) {
+            //     token.id = user.id;
+            //     token.accessToken = newAccount?.access_token;
+            // }
+
+            // // トークンが存在する場合（サインイン後）
+            // if (account?.provider === "google") {
+            //     token.accessToken = account.access_token;
+            // }
+
+            return token;
+        },
+        async session({ session, token }) {
+            return {
+                ...session,
+                accessToken: token.accessToken
+            };
+        }
+    },
+    debug: true
 })
