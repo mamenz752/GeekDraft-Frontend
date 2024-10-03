e8e3f20ccedc7884583f28c01b933ffed59d15af
mamenz752 <mamenz752@gmail.com>
2024-10-03 23:34:41 +0900

    wip: サーバーサイドでの認証

--- auth.ts
+++ auth.ts
@@ -12,6 +12,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         strategy: "jwt"
     },
     callbacks: {
+        async signIn({ account, profile }) {
+            const res = await fetch("http://localhost:3000/api/v1/signin", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    token: account?.access_token,
+                    email: profile?.email,
+                    name: profile?.name,
+                    image: profile?.picture
+                })
+            })
+
+            if (res.ok) {
+                return true;
+            } else {
+                return false;
+            }
+        },
         async jwt({ token, account }) {
             if (account) {
                 token.accessToken = account.access_token;
