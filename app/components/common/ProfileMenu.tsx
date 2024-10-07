b315f435cf5ca1ed086b2e68b83f848b0a7d6333
mamenz752 <mamenz752@gmail.com>
2024-10-07 14:18:10 +0900

    feat: Logoutボタンの実装

--- app/components/common/ProfileMenu.tsx
+++ app/components/common/ProfileMenu.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import React, { FC }  from 'react'
+import LogoutIcon from './icons/LogoutIcon';
+import { signOut } from 'next-auth/react';
 
 type Props = {
     isProfileMenuOpen: boolean;
@@ -28,17 +30,17 @@ const ProfileMenu: FC<Props> = ({ isProfileMenuOpen }) => {
                     </Link>
                 </li>
                 <li>
-                    <Link href="#" className="flex items-center text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500 group">
-                        <span className="sr-only">Newsletter</span>
-                        <svg className="w-3 h-3 me-2 text-gray-400 dark:text-gray-500 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-                        </svg>
-                        ログアウト
-                    </Link>
+                    <button
+                        className="flex items-center gap-2 font-bold text-geek-red-60 hover:opacity-60 group"
+                        onClick={() => signOut({ callbackUrl: '/' })}
+                    >
+                        <span className="sr-only">Logout</span>
+                        <LogoutIcon />
+                        <p>ログアウト</p>
+                    </button>
                 </li>
             </ul>
         </div>
-        
     </div>
   )
 }
--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -1,35 +1,22 @@
 "use client"
 
-import { useSession, signOut } from "next-auth/react"
-import Link from "next/link"
+import { useSession } from "next-auth/react"
 
 const Dashboard = () => {
   const {data: session} = useSession();
 
   if (session) {
     return (
-      <div className="absolute top-24 z-10">
+      <div className="absolute top-24">
           <p>login: {session?.user?.name}</p>
           <p>email: {session?.user?.email}</p>
-          <Link href="/login">
-          <button
-            className="p-8"
-            onClick={() => {
-              // "use server"
-              signOut()
-            }
-          }
-          >
-              ログアウト
-          </button>
-          </Link>
       </div>
     )
   }
 
   return (
     <div className="absolute top-24">
-     <p>Not login</p>
+      <p>Not login</p>
     </div>
   )
 }
