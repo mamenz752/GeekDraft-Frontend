9339c7b416cadef9d452fa6169a03d5fbb7286f9
mamenz752 <mamenz752@gmail.com>
2024-10-07 14:28:26 +0900

    feat: ProfileMenuの実装

--- app/components/common/ProfileMenu.tsx
+++ app/components/common/ProfileMenu.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React, { FC }  from 'react'
+import UserIcon from './icons/UserIcon';
 import LogoutIcon from './icons/LogoutIcon';
 import { signOut } from 'next-auth/react';
 
@@ -21,17 +22,15 @@ const ProfileMenu: FC<Props> = ({ isProfileMenuOpen }) => {
         <div className="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
             <ul className="space-y-4">
                 <li>
-                    <Link href="#" className="flex items-center text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500 group">
-                        <span className="sr-only">Newsletter</span>
-                        <svg className="w-3 h-3 me-2 text-gray-400 dark:text-gray-500 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-                        </svg>
-                        プロフィール
+                    <Link href="/profile" className="flex items-center gap-4 text-geek-gray-60 hover:opacity-60 group">
+                        <span className="sr-only">Profile</span>
+                        <UserIcon />
+                        <p>プロフィール</p>
                     </Link>
                 </li>
                 <li>
                     <button
-                        className="flex items-center gap-2 font-bold text-geek-red-60 hover:opacity-60 group"
+                        className="flex items-center gap-4 font-bold text-geek-red-60 hover:opacity-60 group"
                         onClick={() => signOut({ callbackUrl: '/' })}
                     >
                         <span className="sr-only">Logout</span>
