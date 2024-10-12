09c0d30f414b06cd50f17af366e776675d19e105
mamenz752 <mamenz752@gmail.com>
2024-10-12 11:26:02 +0900

    feat: ログイン時のユーザー登録処理

--- auth.ts
+++ auth.ts
@@ -2,5 +2,30 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [ Google ],
+    providers: [
+        Google({
+            authorization: {params: {access_type: "offline", prompt: "consent"}}
+        })
+    ],
+    pages: {
+        signIn: "/login"
+    },
+    callbacks: {
+        async signIn({ user, account }) {
+            const res = await fetch("http://localhost:8000/api/v1/users", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: user?.email,
+                    name: user?.name,
+                    provider: account?.provider,
+                })
+            });
+            const data = await res.json();
+            return data.success;
+        },
+    },
+    debug: true
 })
